482207130b57e64a9b91970a4105fea7b497ff2f
Matthias Deeg <matthias.deeg@syss.de>
2020-01-30 08:26:35 +0100

    initial public commit

---
+++ Makefile
@@ -0,0 +1,59 @@
+PROJ = icebreaker-glitcher
+ADD_SRC = src/top.v src/pll.v src/duration_counter.v src/offset_counter.v src/resetter.v src/command_processor.v src/fifo.v src/fifo_sync_ram.v src/ram_sdp.v src/uart_defs.v src/uart_rx.v src/uart_tx.v
+
+ADD_CLEAN = *.vcd *_tb base.log
+
+PIN_DEF = syn/icebreaker.pcf
+DEVICE = up5k
+FREQ = 36
+
+all:	$(PROJ).rpt $(PROJ).bin
+
+%.blif:	$(ADD_SRC) $(ADD_DEPS) $(ADD_BOARD_SRC)
+	yosys -ql $*.log -p 'synth_ice40 -top top -blif $@' $(ADD_SRC) $(ADD_BOARD_SRC)
+
+%.json:	$(ADD_SRC) $(ADD_DEPS) $(ADD_BOARD_SRC)
+	yosys -ql $*.log -p 'synth_ice40 -top top -json $@' $(ADD_SRC) $(ADD_BOARD_SRC)
+
+ifeq ($(USE_ARACHNEPNR),)
+%.asc: $(PIN_DEF) %.json
+	nextpnr-ice40 --$(DEVICE) --json $(filter-out $<,$^) --freq $(FREQ) --package sg48 --pcf $< --asc $@
+else
+%.asc: $(PIN_DEF) %.blif
+	arachne-pnr -d $(subst up,,$(subst hx,,$(subst lp,,$(DEVICE)))) -o $@ -p $^
+endif
+
+
+%.bin: %.asc
+	icepack $< $@
+
+%.rpt: %.asc
+	icetime -d $(DEVICE) -mtr $@ $<
+
+%_tb: sim/%_tb.v $(ADD_SRC) $(ADD_TB_SRC)
+	iverilog -o $@ $^
+
+%_tb.vcd: %_tb
+	vvp -N $< +vcd=$@
+
+%_syn.v: %.blif
+	yosys -p 'read_blif -wideports $^; write_verilog $@'
+
+%_syntb: sim/%_tb.v %_syn.v
+	iverilog -o $@ $^ `yosys-config --datdir/ice40/cells_sim.v`
+
+%_syntb.vcd: %_syntb
+	vvp -N $< +vcd=$@
+
+prog:	$(PROJ).bin
+	iceprog $<
+
+sudo-prog:	$(PROJ).bin
+	@echo 'Executing prog as root!!!'
+	sudo iceprog $<
+
+clean:
+	rm -f $(PROJ).blif $(PROJ).asc $(PROJ).rpt $(PROJ).bin $(PROJ).json $(PROJ).log $(ADD_CLEAN)
+
+.SECONDARY:
+.PHONY: all prog clean
---
+++ src/pll.v
@@ -0,0 +1,73 @@
+/*
+  iCEBreaker Glitcher (pll.v)
+ 
+  by Matthias Deeg (@matthiasdeeg, matthias.deeg@syss.de)
+
+  Simple voltage glitcher for an iCEBreaker FPGA
+
+  This glitcher is based on and inspired by glitcher implementations
+  by Dmitry Nedospasov (@nedos) from Toothless Consulting and
+  Grazfather (@Grazfather)
+
+  References:
+    https://www.crowdsupply.com/1bitsquared/icebreaker-fpga
+    https://github.com/toothlessco/arty-glitcher
+    https://toothless.co/blog/bootloader-bypass-part1/
+    https://toothless.co/blog/bootloader-bypass-part2/
+    https://toothless.co/blog/bootloader-bypass-part3/
+    https://github.com/Grazfather/glitcher
+    http://grazfather.github.io/re/pwn/electronics/fpga/2019/12/08/Glitcher.html
+
+  Copyright (C) 2020, Matthias Deeg, SySS GmbH
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its contributors
+     may be used to endorse or promote products derived from this software
+     without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+*/
+
+`timescale 1ns / 1ps
+
+module pll(
+	input  clock_in,
+	output clock_out,
+	output locked
+	);
+
+    // 36 MHz clock
+    SB_PLL40_PAD #(
+		.FEEDBACK_PATH("SIMPLE"),
+		.DIVR(4'b0000),             // DIVR =  0
+		.DIVF(7'b0101111),	        // DIVF = 47
+		.DIVQ(3'b100),		        // DIVQ =  4
+		.FILTER_RANGE(3'b001)	    // FILTER_RANGE = 1
+	) uut (
+		.LOCK(locked),
+        .RESETB(1'b1),
+        .BYPASS(1'b0),
+        .PACKAGEPIN(clock_in),
+        .PLLOUTCORE(clock_out)
+    );
+
+endmodule
